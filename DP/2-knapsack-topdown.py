6a8d3ee8ec81c14c3c2b0bb1ae5b27c7b3fecf38
unknown <nstsrka04@gmail.com>
2025-01-29 23:00:48 +0530

    knapsack top down

--- DP/2-knapsack-topdown.py
+++ DP/2-knapsack-topdown.py
@@ -7,11 +7,14 @@ val = [1, 4, 5, 7]
 
 t = [[-1 for _ in range(W+1)] for _ in range(n+1)]
 
+
+# Base Condtion of Recursive Code is Initialisation in DP
 for i in range(n+1):
     for j in range(W+1):
         if i == 0 or j == 0:
             t[i][j] = 0
 
+# Filling the dp table
 for i in range(n+1):
     for j in range(W+1):
         if wt[i-1] <= j:
