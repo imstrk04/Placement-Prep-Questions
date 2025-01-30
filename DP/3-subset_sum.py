10c81a24c9b99209e984f7f79a274483087efd50
unknown <nstsrka04@gmail.com>
2025-01-30 18:57:44 +0530

    subsetsum + DP Readme

---
+++ DP/3-subset_sum.py
@@ -0,0 +1,44 @@
+# Subset Sum
+'''
+If sum is present in a subset, return True
+'''
+# Knapsack -> wt arr, val arr, W
+# Subset Sum -> arr ~= wt arr and sum ~= W 
+
+
+def subsetSum(arr, sum):
+    n = len(arr)
+    # Intialisation
+    # Along Column => arr size, Along Row => Sum
+    t = [[False for _ in range(sum + 1)] for _ in range(n + 1)]
+
+    for i in range(n + 1):
+        t[i][0] = True
+
+    for i in range(len(arr) + 1):
+        for j in range(sum + 1):
+            if i == 0:
+                t[i][j] = False
+            if j == 0:
+                t[i][j] = True
+    
+    # Knapsack Wt arr => arr []
+    # Knapsack W => sum
+
+    for i in range(1, len(arr) + 1):
+        for j in range(1,sum + 1):
+            if arr[i-1] <= j:
+                t[i][j] = t[i-1][j-arr[i-1]] or t[i-1][j]
+
+            else:
+                t[i][j] = t[i-1][j]
+
+    
+    print("ans:", t[len(arr)][sum])
+
+
+arr = [2, 3, 7, 8, 10]
+sum = 22
+
+
+subsetSum(arr, sum)
---
+++ DP/README.md
@@ -0,0 +1,7 @@
+## DP Tips here n there:
+
+- Write Recursive Code -> Memoization -> Top Down
+- Try to represent the problem in terms of index
+- Do all possible stuffs on that index according to the PS
+- sum up all stuffs -> if q is count all ways,
+- min(of all stuffs) -> for min ways 
